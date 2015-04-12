5bb2b2f08de90db5bf9358227c6dea587daf7b01
Zafar Takhirov <cc.rafaz@zafar.cc>
2015-04-12 02:04:54 -0700

    Finished Part 7 of Derek Banas

--- app/src/main/java/cc/zafar/convertme/MainActivity.java
+++ app/src/main/java/cc/zafar/convertme/MainActivity.java
@@ -4,14 +4,79 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+	private Spinner unitTypeSpinner;
+	private EditText amountTextView;
+
+	private TextView
+		teaspoonTextView, tablespoonTextView, cupTextView, ounceTextView,
+		pintTextView, quartTextView, gallonTextView, poundTextView,
+		milliliterTextView, literTextView, milligramTextView, kilogramTextView;
 
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		addItemsToUnitTypeSpinner();
+
+		addListenerToUnitTypeSpinner();
+
+		amountTextView = (EditText)findViewById(R.id.amount_text_view);
+
+		initializeTextViews();
+	}
+
+	private void addListenerToUnitTypeSpinner () {
+		unitTypeSpinner = (Spinner) findViewById(R.id.unit_type_spinner);
+		unitTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+			@Override
+			public void onItemSelected (AdapterView<?> parent, View view, int position, long id) {
+				String itemSelectedInSpinner = parent.getItemAtPosition(position).toString();
+
+				checkIfConvertingFromTsp(itemSelectedInSpinner);
+			}
+
+			@Override
+			public void onNothingSelected (AdapterView<?> parent) {
+				// TODO: Add something here later! :)
+			}
+		});
+	}
+
+	private void addItemsToUnitTypeSpinner () {
+		unitTypeSpinner = (Spinner) findViewById(R.id.unit_type_spinner);
+		ArrayAdapter<CharSequence> unitTypeSpinnerAdapter
+			= ArrayAdapter.createFromResource(this,
+			                                  R.array.conversion_types,
+			                                  android.R.layout.simple_spinner_item);
+
+		unitTypeSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		unitTypeSpinner.setAdapter(unitTypeSpinnerAdapter);
+	}
+
+	private void initializeTextViews () {
+		teaspoonTextView = (TextView) findViewById(R.id.tsp_text_view);
+		tablespoonTextView = (TextView) findViewById(R.id.tbs_text_view);
+		cupTextView = (TextView) findViewById(R.id.cup_text_view);
+		ounceTextView = (TextView) findViewById(R.id.oz_text_view);
+		pintTextView = (TextView) findViewById(R.id.pint_text_view);
+		quartTextView = (TextView) findViewById(R.id.quart_text_view);
+		gallonTextView = (TextView) findViewById(R.id.gallon_text_view);
+		poundTextView = (TextView) findViewById(R.id.pound_text_view);
+		milliliterTextView = (TextView) findViewById(R.id.ml_text_view);
+		literTextView = (TextView) findViewById(R.id.liter_text_view);
+		milligramTextView = (TextView) findViewById(R.id.mg_text_view);
+		kilogramTextView = (TextView) findViewById(R.id.kg_text_view);
 	}
 
 
