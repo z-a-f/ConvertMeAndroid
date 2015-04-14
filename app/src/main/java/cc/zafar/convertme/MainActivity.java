c6b327154f19561eb8c03d7a0260cb26ec307ee5
Zafar Takhirov <cc.rafaz@zafar.cc>
2015-04-14 01:19:27 -0700

    FInished :)

--- app/src/main/java/cc/zafar/convertme/MainActivity.java
+++ app/src/main/java/cc/zafar/convertme/MainActivity.java
@@ -1,7 +1,7 @@
 package cc.zafar.convertme;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -43,7 +43,8 @@ public class MainActivity extends ActionBarActivity {
 			public void onItemSelected (AdapterView<?> parent, View view, int position, long id) {
 				String itemSelectedInSpinner = parent.getItemAtPosition(position).toString();
 
-				checkIfConvertingFromTsp(itemSelectedInSpinner);
+				// checkIfConvertingFromTsp(itemSelectedInSpinner);
+				updateValues(itemSelectedInSpinner);
 			}
 
 			@Override
@@ -52,6 +53,128 @@ public class MainActivity extends ActionBarActivity {
 			}
 		});
 	}
+
+	private Quantity.Unit SpinnerToUnit (String currentUnit) {
+		switch (currentUnit) {
+			case "teaspoon": return Quantity.Unit.tsp;
+			case "tablespoon": return Quantity.Unit.tbs;
+			case "cup": return Quantity.Unit.cup;
+			case "ounce": return Quantity.Unit.oz;
+			case "pint": return Quantity.Unit.pint;
+			case "quart": return Quantity.Unit.quart;
+			case "gallon": return Quantity.Unit.gallon;
+			case "pound": return Quantity.Unit.pound;
+			case "milliliter": return Quantity.Unit.ml;
+			case "liter": return Quantity.Unit.liter;
+			case "milligram": return Quantity.Unit.mg;
+			default: return Quantity.Unit.kg;
+		}
+	}
+
+	private TextView SpinnerToTextView(String currentUnit) {
+		switch (currentUnit) {
+			case "teaspoon": return teaspoonTextView;
+			case "tablespoon": return tablespoonTextView;
+			case "cup": return cupTextView;
+			case "ounce": return ounceTextView;
+			case "pint": return pintTextView;
+			case "quart": return quartTextView;
+			case "gallon": return gallonTextView;
+			case "pound": return poundTextView;
+			case "milliliter": return milliliterTextView;
+			case "liter": return literTextView;
+			case "milligram": return milligramTextView;
+			default: return kilogramTextView;
+		}
+	}
+
+	public void updateValues(String selection /*What units are used as base*/) {
+		// Amount to be converted:
+		double doubleToConvert = Double.parseDouble(amountTextView.getText().toString());
+		TextView currentTextView = SpinnerToTextView(selection);
+		Quantity.Unit currentUnit = SpinnerToUnit(selection);
+		Quantity currentQuantity = new Quantity(doubleToConvert, currentUnit);
+		double teaspoonsToConvert;
+
+
+		// Convert the current value to teaspoons:
+		if (!selection.equals("teaspoon")) {
+			teaspoonsToConvert = currentUnit.toBaseUnit(doubleToConvert);
+		} else {
+			teaspoonsToConvert = doubleToConvert;
+		}
+
+		// Update all fields:
+		updateUnitFieldsUsingTsp(teaspoonsToConvert, Quantity.Unit.tsp, teaspoonTextView);
+		updateUnitFieldsUsingTsp(teaspoonsToConvert, Quantity.Unit.tbs, tablespoonTextView);
+		updateUnitFieldsUsingTsp(teaspoonsToConvert, Quantity.Unit.cup, cupTextView);
+		updateUnitFieldsUsingTsp(teaspoonsToConvert, Quantity.Unit.oz, ounceTextView);
+		updateUnitFieldsUsingTsp(teaspoonsToConvert, Quantity.Unit.pint, pintTextView);
+		updateUnitFieldsUsingTsp(teaspoonsToConvert, Quantity.Unit.quart, quartTextView);
+		updateUnitFieldsUsingTsp(teaspoonsToConvert, Quantity.Unit.gallon, gallonTextView);
+		updateUnitFieldsUsingTsp(teaspoonsToConvert, Quantity.Unit.pound, poundTextView);
+		updateUnitFieldsUsingTsp(teaspoonsToConvert, Quantity.Unit.ml, milliliterTextView);
+		updateUnitFieldsUsingTsp(teaspoonsToConvert, Quantity.Unit.liter, literTextView);
+		updateUnitFieldsUsingTsp(teaspoonsToConvert, Quantity.Unit.mg, milligramTextView);
+		updateUnitFieldsUsingTsp(teaspoonsToConvert, Quantity.Unit.kg, kilogramTextView);
+		// Return the base field to original:
+		currentTextView.setText(currentQuantity.toString());
+	}
+
+	public void updateUnitFieldsUsingTsp (double numberOfTeaspoons,
+	                                     Quantity.Unit unitConvertingTo, TextView theTextView) {
+		Quantity unitQuantity = new Quantity(numberOfTeaspoons, Quantity.Unit.tsp);
+		String tempUnit = unitQuantity.to(unitConvertingTo).toString();
+		theTextView.setText(tempUnit);
+
+	}
+
+	/*
+	private void checkIfConvertingFromTsp (String currentUnit) {
+		double doubleToConvert = Double.parseDouble(amountTextView.getText().toString());
+
+		if (currentUnit.equals("teaspoon")) {
+			updateUnitTypeUsingTsp(Quantity.Unit.tsp, doubleToConvert);
+		} else {
+			updateUnitTypesUsingOther(SpinnerToUnit(currentUnit));
+
+		}
+
+	}
+
+	public void updateUnitTypesUsingOther (Quantity.Unit currentUnit) {
+		double doubleToConvert = Double.parseDouble(amountTextView.getText().toString());
+		Quantity currentQuantitySelected = new Quantity(doubleToConvert, currentUnit);
+		String valueInTeaspoons = currentQuantitySelected.to(Quantity.Unit.tsp).toString();
+	}
+
+	public void updateUnitTypeUsingTsp (Quantity.Unit currentUnit, double doubleToConvert) {
+		// double doubleToConvert = Double.parseDouble(amountTextView.getText().toString());
+		String teaspoonValueAndUnit = doubleToConvert + " tsp";
+		teaspoonTextView.setText(teaspoonValueAndUnit);
+		updateUnitTextFieldsUsingTsp(doubleToConvert, Quantity.Unit.tbs, tablespoonTextView);
+		updateUnitTextFieldsUsingTsp(doubleToConvert, Quantity.Unit.cup, cupTextView);
+		updateUnitTextFieldsUsingTsp(doubleToConvert, Quantity.Unit.oz, ounceTextView);
+		updateUnitTextFieldsUsingTsp(doubleToConvert, Quantity.Unit.pint, pintTextView);
+		updateUnitTextFieldsUsingTsp(doubleToConvert, Quantity.Unit.quart, quartTextView);
+		updateUnitTextFieldsUsingTsp(doubleToConvert, Quantity.Unit.gallon, gallonTextView);
+		updateUnitTextFieldsUsingTsp(doubleToConvert, Quantity.Unit.pound, poundTextView);
+		updateUnitTextFieldsUsingTsp(doubleToConvert, Quantity.Unit.ml, milliliterTextView);
+		updateUnitTextFieldsUsingTsp(doubleToConvert, Quantity.Unit.liter, literTextView);
+		updateUnitTextFieldsUsingTsp(doubleToConvert, Quantity.Unit.mg, milligramTextView);
+		updateUnitTextFieldsUsingTsp(doubleToConvert, Quantity.Unit.kg, kilogramTextView);
+	}
+
+	public void updateUnitTextFieldsUsingTsp (double doubleToConvert,
+	                                          Quantity.Unit unitConvertingTo,
+	                                          TextView theTextView) {
+		Quantity unitQuantity = new Quantity(doubleToConvert, Quantity.Unit.tsp);
+		String tempUnit = unitQuantity.to(unitConvertingTo).toString();
+		theTextView.setText(tempUnit);
+
+	}
+	*/
+
 
 	private void addItemsToUnitTypeSpinner () {
 		unitTypeSpinner = (Spinner) findViewById(R.id.unit_type_spinner);
